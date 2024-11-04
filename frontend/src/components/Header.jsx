3693353844e60d52bbd10e031e43343bddb8a386
Michel Arendt <kingdomx@gmail.com>
2024-11-04 10:42:19 -0300

    Code cleanup

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,28 +9,11 @@ import Svg from '/src/components/shared/Svg';
 import logo from '/assets/logo_only_text.png';
 
 const Header = () => {
-	const [open, setOpen] = useState('')
 	const { isOpen, toggleMenu } = useMenuStore();
-
-	// Animation
-  const [showList, setShowList] = useState(false);
-  const [triggerAnimation, setTriggerAnimation] = useState(false);
-  const toggleAnimation = () => {
-    // Temporarily remove show-list class to reset animation
-    setTriggerAnimation(false);
-    setTimeout(() => {
-      setTriggerAnimation(true); // Reapply show-list class after delay
-      setShowList((prev) => !prev);
-    }, 10); // Small delay to reset animation
-  };
 
 	// Store references to elements using useRef
 	const navRef = useRef(null);
 	const closeButtonRef = useRef(null);
-
-	const navigationClicked = () => {
-		setOpen((prev) => (prev === '' ? 'open' : ''));
-	};
 
 	useEffect(() => {
     if (window.innerWidth > 900) {
@@ -89,30 +72,6 @@ const Header = () => {
 								</DropdownSubmenu>
 							</Dropdown>
 						</li>
-
-
-						{/* <li className='dropdown__container'>
-							<Link onClick={toggleAnimation}>
-								<Svg type="shopping_bag" sizes={[16,16]} className='display__hide_on-desktop' />
-								<span>Produtos</span>
-								<Svg type="arrow_drop_down" sizes={[10,10]} className='display__hide_on-mobile' />
-							</Link>
-							<ul className={`dropdown__submenu ${triggerAnimation  ? 'show-list' : ''}`}>
-								<li style={{ animationDelay: "0s" }}><Link to='/produtos'>Bolsas</Link></li>
-								<li style={{ animationDelay: "0.1s" }}><Link to='/produtos'>Bolsas Maternidade</Link></li>
-								<li style={{ animationDelay: "0.2s" }}><Link to='/produtos'>Bolsas Ecológicas</Link></li>
-								<li style={{ animationDelay: "0.3s" }}><Link to='/produtos'>Bolsas Térmicas</Link></li>
-								<li style={{ animationDelay: "0.4s" }}><Link to='/produtos'>Bolsas Viagem</Link></li>
-								<li style={{ animationDelay: "0.5s" }}><Link to='/produtos'>Estojos</Link></li>
-								<li style={{ animationDelay: "0.6s" }}><Link to='/produtos'>Necessaires</Link></li>
-								<li style={{ animationDelay: "0.7s" }}><Link to='/produtos'>Malotes</Link></li>
-								<li style={{ animationDelay: "0.8s" }}><Link to='/produtos'>Mochilas</Link></li>
-								<li style={{ animationDelay: "0.9s" }}><Link to='/produtos'>Mochilas Saco</Link></li>
-								<li style={{ animationDelay: "1s" }}><Link to='/produtos'>Shoulder Bags</Link></li>
-							</ul>
-						</li> */}
-
-
 						<li>
 							<Link to="/">
 								<Svg type="email" sizes={[16,16]} className='display__hide_on-desktop' />
