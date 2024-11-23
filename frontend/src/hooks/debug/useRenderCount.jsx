f48cca983aeac572f8253cd8457fd8d9bb70bfd3
Michel Arendt <kingdomx@gmail.com>
2024-11-23 07:28:57 -0300

    useRenderCount should not run in production

--- frontend/src/hooks/debug/useRenderCount.jsx
+++ frontend/src/hooks/debug/useRenderCount.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useRef } from 'react';
 
 const useRenderCount = (component) => {
+  if (process.env.NODE_ENV === 'production') {
+    return; // Do nothing in production, code will be removed by Webpack
+  }
+
   const renderCount = useRef(0);
 
   useEffect(() => {
-    renderCount.current += 1; // Increment the render count
-    console.log(`[${component.name}] Render Count: ${renderCount.current}`);
+    renderCount.current += 1;
+    console.log(`[${component.name.toUpperCase()}] Render Count: ${renderCount.current}`);
   });
 };
 
