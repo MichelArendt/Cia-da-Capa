519df6d94148511629d8bc3b2b28451057099b00
Michel Arendt <kingdomx@gmail.com>
2025-01-28 04:52:37 -0300

    FRONTEND: product add pages

--- frontend/Services/API/ProductService.cs
+++ frontend/Services/API/ProductService.cs
@@ -1,4 +1,9 @@
-﻿namespace frontend.Services.API
+﻿using frontend.Constants;
+using frontend.DTOs;
+using frontend.Helpers;
+using System.Net.Http.Json;
+
+namespace frontend.Services.API
 {
     public class ProductService
     {
@@ -8,6 +13,17 @@
         public ProductService(HttpClient httpClient)
         {
             _httpClient = httpClient;
+        }
+
+        public async Task<List<ProductDto>> GetProductsAsync()
+        {
+            var response = await _httpClient.GetAsync(ApiEndpoints.Public.Product.GetAll);
+
+            return await ApiServiceHelper.DeserializeResponse<List<ProductDto>>(response);
+        }
+        public async Task<HttpResponseMessage> CreateNewProductAsync(NewProductDto product)
+        {
+            return await _httpClient.PostAsJsonAsync(ApiEndpoints.Manage.Product.Create, product);
         }
     }
 }
