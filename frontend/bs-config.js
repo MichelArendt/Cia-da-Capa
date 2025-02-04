4b3c1ba7bdce3e2d9659168a58460afffa343c13
Michel Arendt <kingdomx@gmail.com>
2025-02-04 13:55:41 -0300

    Fixed broswer sync and new after publish target to remove SRI from index.html

--- frontend/bs-config.js
+++ frontend/bs-config.js
@@ -1,21 +1,9 @@
 ﻿module.exports = {
-    proxy: "http://localhost:5038", // Proxy your .NET app
-    port: 3000, // Serve on port 3000
-    files: ["wwwroot/css/**/*.css"], // Watch for changes in CSS
-    serveStatic: ["wwwroot"], // Serve static files from 'wwwroot'
-    middleware: [
-        {
-            route: "/api",
-            handle: (req, res, next) => {
-                const httpProxy = require("http-proxy");
-                const proxy = httpProxy.createProxyServer({
-                    target: "http://localhost:80", // Proxy API calls to the API server
-                    changeOrigin: true,
-                });
-                proxy.web(req, res, next);
-            },
-        },
-    ],
+    proxy: "http://localhost",
+    port: 3000,
+    files: ["../build/**/*.{css,scss,js,html}"],
+    serveStatic: ["../build"],
     open: false,
     notify: false,
+    logLevel: "debug",
 };
