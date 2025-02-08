716c79920a6ecf685739984b23867f45769becce
Michel Arendt <kingdomx@gmail.com>
2025-02-08 08:10:40 -0300

    Initial code for tabbed content UI component

---
+++ frontend/ViewModels/TabbedNavigation/TabViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace frontend.ViewModels.TabbedNavigation
+{
+    public class TabViewModel
+    {
+        public required string Title { get; set; }
+        public required RenderFragment Content { get; set; }
+        public bool Visible { get; set; } = false;
+    }
+}
