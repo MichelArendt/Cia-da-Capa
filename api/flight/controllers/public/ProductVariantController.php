6d021864c1f03f38992a18a7d7cca8550a4f8c20
Michel Arendt <kingdomx@gmail.com>
2025-02-08 06:48:20 -0300

    API: ErrorHandler now halts execution

--- api/flight/controllers/public/ProductVariantController.php
+++ api/flight/controllers/public/ProductVariantController.php
@@ -9,20 +9,15 @@ use Helpers\ErrorHandler;
 class ProductVariantController {
   public function getVariantsForProductWithId($id){
     try {
-      error_log("ProductVariantController->getVariantsForProductWithId 1");
       $productVariantModel = Flight::get('productVariantModel');
       $productVariants = $productVariantModel->getVariantsForProductWithId($id);
-      error_log("ProductVariantController->getVariantsForProductWithId 2");
 
       if($productVariants === null) {
-        error_log("ProductVariantController->getVariantsForProductWithId 22");
         throw new Exception("Failed to retrieve product categories.");
       }
 
-      error_log("ProductVariantController->getVariantsForProductWithId 3");
       Flight::json($productVariants, 200);
     } catch (Exception $e) {
-      error_log("ProductVariantController->getVariantsForProductWithId 4");
       ErrorHandler::handleException($e, __METHOD__);
     }
   }
--- api/flight/helpers/ErrorHandler.php
+++ api/flight/helpers/ErrorHandler.php
@@ -6,17 +6,33 @@ use Flight;
 
 class ErrorHandler
 {
-    public static function handleException(\Exception $e, string $methodName)
+    public static function handleException(\Exception $e, string $methodName, string $message = "Erro no servidor.")
     {
-        $message = "Error in {$methodName}: " . $e->getMessage();
-        error_log($message);
-        Flight::json(["message" => $message], 500);
+        // Format the error message
+        $formattedError = "Error in {$methodName}: " . $e->getMessage();
+
+        // Log the error
+        error_log($formattedError);
+
+        // Stop execution and return a JSON response
+        Flight::halt(500, json_encode([
+            "error" => $formattedError,
+            "message" => $message
+        ]));
     }
 
-    public static function logError(string $customMessage, string $methodName, int $code = 500)
+    public static function logError(string $error, string $methodName, string $message = "Erro no servidor.", int $code = 500)
     {
-        $message = "Error in {$methodName}: " . $customMessage;
-        error_log($message);
-        Flight::json(["message" => $message], $code);
+        // Format the error message
+        $formattedError  = "Error in {$methodName}: " . $error;
+
+        // Log the error
+        error_log($formattedError);
+
+        // Stop execution and return a JSON response
+        Flight::halt($code, json_encode([
+            "error" => $formattedError,
+            "message" => $message
+        ]));
     }
 }
