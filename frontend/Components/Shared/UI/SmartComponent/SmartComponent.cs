b317df279421f3217a234a8805cf63201483a5c0
Michel Arendt <kingdomx@gmail.com>
2025-02-18 11:13:36 -0300

    Fixed some UI synchronization issues

--- frontend/Components/Shared/UI/SmartComponent/SmartComponent.cs
+++ frontend/Components/Shared/UI/SmartComponent/SmartComponent.cs
@@ -1,15 +1,60 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using frontend.Services;
+using Microsoft.AspNetCore.Components;
 
 namespace frontend.Components.Shared.UI.SmartComponent
 {
-    public abstract class SmartComponentBase : ComponentBase
+    public abstract class SmartComponentBase : ComponentBase, IDisposable
     {
         protected bool IsOpen { get; private set; }
 
-        public void Open() => IsOpen = false;
+        [Inject]
+        protected AppStateService AppState { get; set; } = default!;
 
-        public void Close() => IsOpen = false;
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
 
-        public void Toggle() => IsOpen = !IsOpen;
+            AppState.NavigationChanged += Close;
+            AppState.IsMobileChanged += Close;
+        }
+
+        public virtual void Dispose()
+        {
+            AppState.NavigationChanged -= Close;
+            AppState.IsMobileChanged -= Close;
+        }
+
+        public void Open()
+        {
+            IsOpen = true;
+            StateHasChanged();
+        }
+
+        public void Close()
+        {
+            IsOpen = false;
+            StateHasChanged();
+        }
+
+        //public async Task CloseAsync()
+        //{
+        //    Close();
+        //    await Task.Yield();
+        //}
+
+        public void Toggle()
+        {
+            IsOpen = !IsOpen;
+            StateHasChanged();
+        }
+
+        public void ToggleOnlyOnMobile()
+        {
+            if (AppState.IsMobile == true)
+            {
+                IsOpen = !IsOpen;
+                StateHasChanged();
+            }
+        }
     }
 }
