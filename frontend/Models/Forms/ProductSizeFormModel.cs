f422ec7b9c347a18e230add14738bc1f01f922b5
Michel Arendt <kingdomx@gmail.com>
2025-02-27 09:30:19 -0300

    Fixes ProductSizeForm not capturing validation errors for integers

--- frontend/Models/Forms/ProductSizeFormModel.cs
+++ frontend/Models/Forms/ProductSizeFormModel.cs
@@ -5,18 +5,18 @@ namespace frontend.Models.Forms
     public class ProductSizeFormModel
     {
         [Display(Name = "ID do Rótulo de Tamanho")]
-        public int SizeLabelId { get; set; }
+        public int? SizeLabelId { get; set; }
 
         [Display(Name = "Largura (cm)")]
         [Required(ErrorMessage = "Largura é obrigatória.")]
-        public float Width { get; set; }
+        public float? Width { get; set; }
 
         [Display(Name = "Altura (cm)")]
         [Required(ErrorMessage = "Altura é obrigatória.")]
-        public float Height { get; set; }
+        public float? Height { get; set; }
 
         [Display(Name = "Profundidade (cm)")]
         [Required(ErrorMessage = "Profundidade é obrigatória.")]
-        public float Depth { get; set; }
+        public float? Depth { get; set; }
     }
 }
