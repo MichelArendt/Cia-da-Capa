63baf58ff02d1e9f537bde0079df78536c744a81
Michel Arendt <kingdomx@gmail.com>
2025-02-28 09:38:34 -0300

    ProductSizeForm changed to adhere to new styling pattern

--- frontend/Models/Forms/ProductSizeLabelFormModel.cs
+++ frontend/Models/Forms/ProductSizeLabelFormModel.cs
@@ -6,12 +6,14 @@ namespace frontend.Models.Forms
     public class ProductSizeLabelFormModel
     {
         public int Id { get; set; }
+
         [Display(Name = "Título do Rótulo de Tamanho")]
         [Required(ErrorMessage = "Título é obrigatório.")]
         public string Title { get; set; } = string.Empty;
 
         [Display(Name = "Rótulo de Tamanho")]
         [Required(ErrorMessage = "Rótulo é obrigatório.")]
+        [MinLength(1, ErrorMessage = "O rótulo deve ter pelo menos 1 caractere.")]
         public string Label { get; set; } = string.Empty;
 
         public void FromDto(ProductSizeLabelDto dto)
