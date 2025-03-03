8379841214b66c4b4d670566b85428a1fd0645ec
Michel Arendt <kingdomx@gmail.com>
2025-03-03 13:27:51 -0300

    Disabled Blazor's default service worker

--- frontend/wwwroot/service-worker.js
+++ frontend/wwwroot/service-worker.js
@@ -1,4 +1,15 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
+
+self.addEventListener('install', event => {
+    // Immediately activate the new service worker
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+// In development, always fetch from the network.
 self.addEventListener('fetch', () => { });
