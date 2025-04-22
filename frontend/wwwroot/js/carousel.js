3fe7766629a70575098b0a1d6498efa2ad5e00d8
Michel Arendt <kingdomx@gmail.com>
2025-04-22 15:34:16 -0300

    Initial splash & carousel for home page

---
+++ frontend/wwwroot/js/carousel.js
@@ -0,0 +1,10 @@
+﻿window.scrollCarousel = (element, toRight) => {
+    const distance = 200;
+
+    if (element) {
+        element.scrollBy({
+            left: toRight ? distance : -distance,
+            behavior: 'smooth'
+        });
+    }
+};
