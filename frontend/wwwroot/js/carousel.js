3082ea17c610880f5f45e87003a9237b83da1d99
Michel Arendt <kingdomx@gmail.com>
2025-04-24 05:47:53 -0300

    Carousel autoscroll through items

--- frontend/wwwroot/js/carousel.js
+++ frontend/wwwroot/js/carousel.js
@@ -1,10 +1,24 @@
-﻿window.scrollCarousel = (element, toRight) => {
-    const distance = 200;
+﻿//window.scrollCarousel = (element, toRight) => {
+//    const distance = 200;
 
-    if (element) {
-        element.scrollBy({
-            left: toRight ? distance : -distance,
-            behavior: 'smooth'
-        });
-    }
+//    if (element) {
+//        element.scrollBy({
+//            left: toRight ? distance : -distance,
+//            behavior: 'smooth'
+//        });
+//    }
+//};
+
+window.scrollCarouselToIndex = function (carouselListRef, index) {
+    if (!carouselListRef || !carouselListRef.children || index >= carouselListRef.children.length)
+        return;
+
+    const itemWidth = carouselListRef.children[0].offsetWidth;
+    const gap = parseInt(getComputedStyle(carouselListRef).gap || 0);
+    const scrollAmount = index * (itemWidth + gap);
+
+    carouselListRef.scrollTo({
+        left: scrollAmount,
+        behavior: 'smooth'
+    });
 };
