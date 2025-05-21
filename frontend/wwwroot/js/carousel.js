f8eb821e5b433973a8725c69d60477d1f7a4b689
Michel Arendt <kingdomx@gmail.com>
2025-05-21 09:08:23 -0300

    Carousel component changed for more smoothness

--- frontend/wwwroot/js/carousel.js
+++ frontend/wwwroot/js/carousel.js
@@ -1,13 +1,22 @@
 ﻿window.scrollCarouselToIndex = function (carouselListRef, index) {
-    if (!carouselListRef || !carouselListRef.children || index >= carouselListRef.children.length)
-        return;
+    //if (!carouselListRef || !carouselListRef.children || index >= carouselListRef.children.length)
+    //    return;
 
-    const itemWidth = carouselListRef.children[0].offsetWidth;
-    const gap = parseInt(getComputedStyle(carouselListRef).gap || 0);
-    const scrollAmount = index * (itemWidth + gap);
+    //const itemWidth = carouselListRef.children[0].offsetWidth;
+    //const gap = parseInt(getComputedStyle(carouselListRef).gap || 0);
+    //const scrollAmount = index * (itemWidth + gap);
 
-    carouselListRef.scrollTo({
-        left: scrollAmount,
-        behavior: 'smooth'
+    //carouselListRef.scrollTo({
+    //    left: scrollAmount,
+    //    behavior: 'smooth'
+    //});
+
+    const item = carouselListRef?.children?.[index];
+    if (!item) return;
+
+    item.scrollIntoView({
+        behavior: 'smooth',
+        inline: 'center',
+        block: 'nearest' // ensures no vertical scroll
     });
 };
