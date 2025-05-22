e39339e5348252bba8de5d8f6ef83d65a983f8f9
Michel Arendt <kingdomx@gmail.com>
2025-05-22 12:09:45 -0300

    New HtmlFormatter and frontend Utils class

---
+++ frontend/Utils/HtmlFormatter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace frontend.Utils
+{
+    public static class HtmlFormatter
+    {
+        /// <summary>
+        /// Converts newlines (\n) to <br /> for safe HTML rendering.
+        /// </summary>
+        /// <param name="text">Text that may contain newlines.</param>
+        public static MarkupString ConvertNewlinesToBr(string? text)
+        {
+            return new MarkupString((text ?? string.Empty).Replace("\n", "<br />"));
+        }
+    }
+}
