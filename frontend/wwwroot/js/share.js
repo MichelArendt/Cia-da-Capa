6cae3c58e3450acb099cbd5433c38b2388d1cd6a
Michel Arendt <kingdomx@gmail.com>
2025-05-30 13:30:42 -0300

    Tweaks to ProductDetails, Carousel, ShareProduct and AddToCart components (including styling changes and fixes)

---
+++ frontend/wwwroot/js/share.js
@@ -0,0 +1,11 @@
+﻿window.canShareNatively = () => {
+    return !!navigator.share;
+};
+
+window.shareProduct = (title, text, url) => {
+    if (navigator.share) {
+        navigator.share({ title, text, url }).catch(() => { });
+    } else {
+        alert("Compartilhamento nativo não suportado neste dispositivo.");
+    }
+};
