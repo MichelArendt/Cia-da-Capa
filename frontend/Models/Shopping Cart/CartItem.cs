0d570b8c9c0050dd585dda4ca54194f09102d2b8
Michel Arendt <kingdomx@gmail.com>
2025-06-03 09:49:19 -0300

    Initial shopping cart setup

---
+++ frontend/Models/Shopping Cart/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using frontend.DTOs;
+
+namespace frontend.Models.Shopping_Cart
+{
+    public class CartItem
+    {
+        public ProductDto Product { get; set; } = default!;
+
+        private int _quantity = 100;
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                _quantity = value < 100 ? 100 : value; // Ensure quantity is at least 100
+            }
+        }
+    }
+}
